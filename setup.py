4b419f2581cfae06c4a90dfcd75aae3b56d1c75d
PeterFujiyu <88922024+PeterFujiyu@users.noreply.github.com>
2023-07-04 16:27:10 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import platform
+from setuptools import setup
+
+# 根据操作系统选择所需的模块
+required_modules = ['watchdog', 'tkinter', 'pycryptodomex']
+if platform.system() == 'Linux':
+    required_modules.append('python-crontab')
+
+setup(
+    name='incremental-backup',
+    version='1.0.0',
+    description='Incremental Backup Script',
+    author='Your Name',
+    author_email='your.email@example.com',
+    packages=[''],
+    install_requires=required_modules,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux' if platform.system() == 'Linux' else 'Operating System :: Microsoft :: Windows',
+    ],
+)
